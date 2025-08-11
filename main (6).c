a40a3005b9353d603db9cc7b8006a342205283e9
Carl0s2009 <carlos2008urbina@gmail.com>
2025-08-11 16:04:32 -0300

    Add files via upload

---
+++ main (6).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char STR [20], STR1[20];
+    
+    printf("Digite uma string:");
+    fgets(STR, sizeof(STR), stdin);
+    printf("Digite outra string:");
+    gets(STR1);
+    if(!SCHP(STR, STR1){
+        printf("Sao Iguais");
+    }
+}
